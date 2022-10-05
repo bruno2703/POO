1f59286de25d0148640911966ad2cae970b2bcea
bruno2703 <107515531+bruno2703@users.noreply.github.com>
2022-10-04 22:20:41 -0300

    update

---
+++ Estressados/D:Controles/test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include "aux.hpp"
+using namespace aux;
+using namespace std;
+
+std::vector<std::pair<int, int>> occurr(std::vector<int> vet) {
+    return {}; // todo
+}
+
+std::vector<std::pair<int, int>> teams(std::vector<int> vet) {
+    return {}; // todo
+}
+
+std::vector<int> mnext(std::vector<int> vet) {
+    return {}; // todo
+}
+
+std::vector<int> alone(std::vector<int> vet) {
+    return {}; // todo
+}
+
+int couple(std::vector<int> vet) {
+    return {}; // todo
+}
+
+bool has_subseq(std::vector<int> vet, std::vector<int> seq, int pos) {
+    return {}; // todo
+}
+
+int subseq(std::vector<int> vet, std::vector<int> seq) {
+    return {}; // todo
+}
+
+std::vector<int> erase(std::vector<int> vet, std::vector<int> pos_list) {
+    return {}; // todo
+}
+
+std::vector<int> clear(std::vector<int> vet, int value) {
+    return {}; // todo
+}
+
+int main(){
+
+
+
+    return 0;
+}
